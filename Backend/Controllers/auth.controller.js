9f267f664d4b9cf51c1fe001db5c962f1de02f6a
jay02124 <106666301+jay02124@users.noreply.github.com>
2024-04-03 21:38:04 +0530

    Changes committed: #    modified:   .env #    new file:   Backend/Controllers/auth.controller.js #    new file:   Backend/Routes/auth.routes.js #    modified:   Backend/server.js #    modified:   package.json

---
+++ Backend/Controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const login = (req, res) => {
+    res.send("login success");
+    console.log("Login");
+};  
+
+export const logout = (req, res) => {
+    res.send("logout success");
+    console.log("Logout");
+};
+
+
+export const signup = (req, res) => {
+    res.send("signup success");
+    console.log("signup");
+};  
--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,19 @@
-const express = require('express');
-
+import  express  from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './Routes/auth.routes.js';
 
 const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+
 app.get('/', (req, res) => {
 //root route http://localhost:5000/
 res.send("hello world!!");
 });
 
+app.use("/api/auth",authRoutes); 
 
-app.listen(5000, () => console.log("listening on port 5000"));
+
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
